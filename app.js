7029242c787f20f467202606e45a5bb71e5756b7
Jessmond Nazarrea <jsmndnzrrea23@users.noreply.github.com>
2018-08-15 16:57:50 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+app.set('port', process.env.PORT || 2502)
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(app.get('port'), function() {
+  console.log('listening to.. ', app.get('port'));
+});
